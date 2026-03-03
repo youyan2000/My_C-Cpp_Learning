e0741d823240dc15e0ced4750d2405dcff117c72
Eugene <woaiyangjichong@gmail.com>
2026-03-03 21:22:34 +0800

    2026-3-3 save

--- helloworld/src/main.cpp
+++ helloworld/src/main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World! I was " << 18 << endl;
+    int age;
+    cin >> age;
+    cout << "And now I'm " << age << endl;
     return 0;
 }
